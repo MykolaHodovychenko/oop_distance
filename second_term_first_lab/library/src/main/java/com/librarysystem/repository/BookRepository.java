d40880a195fc27d3bab43c565ba958da5aa1df88
DariaSyanska <daria.syanska@gmail.com>
2025-04-05 18:20:14 +0200

    Remove lab1 from lab2 branch

--- second_term_first_lab/library/src/main/java/com/librarysystem/repository/BookRepository.java
+++
@@ -1,7 +0,0 @@
-package com.librarysystem.repository;
-
-import com.librarysystem.model.Book;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BookRepository extends JpaRepository<Book, Long> {
-}
--- second_term_first_lab/library/src/main/java/com/librarysystem/repository/LibraryMemberRepository.java
+++
@@ -1,7 +0,0 @@
-package com.librarysystem.repository;
-
-import com.librarysystem.model.LibraryMember;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LibraryMemberRepository extends JpaRepository<LibraryMember, Long> {
-}
